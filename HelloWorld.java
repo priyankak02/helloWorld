7b962f7012dec3b74e2f6c7b99d2d23116942577
priyankak02 <125034116+priyankak02@users.noreply.github.com>
2023-09-07 15:07:34 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello World. Welcome to World of Jenkins. My name is Priyanka");
+		System.out.println("Hello World. Welcome to World of Jenkins.");
 	}
 }
